1ba0e3cbe550b0d3922c03534147c46b33f5509b
Rodrigo Chavez <rodctech@gmail.com>
2019-05-30 16:29:29 -0500

    321- added compDidUpdate

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -5,9 +5,20 @@ import axios from 'axios';
 class ResourceList extends Component {
     state = {resources: [ ]};
    async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        );
 
        this.setState({resources: response.data})
+    }
+
+    async componentDidUpdate(prevProps) {
+       if(prevProps.resource !== this.props.resource) {
+           const response = await axios.get(
+               `https://jsonplaceholder.typicode.com/${this.props.resource}`
+           );
+           this.setState({resources: response.data });
+       }
     }
 
     render() {
