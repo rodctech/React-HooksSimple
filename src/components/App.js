ba06cb2a842f9404a248b3885f5e3bc1f3de3fc6
Rodrigo Chavez <rodctech@gmail.com>
2019-05-30 15:44:06 -0500

    317- Refactor to Functional Comp w/Hook useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, useState } from 'react';
 
-class App extends Component {
-    state = {resource: 'posts'};
-
-    render() {
-
+//class App extends Component {
+  //  state = {resource: 'posts'};
+const App = () => {
+    const [resource, setResource] = useState('posts');
+   // render() {
         return (
             <div>
                 <div>
-                    <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-                    <button onClick={() => this.setState({resource: 'todos'})}>ToDos</button>
+                    <button onClick={() => setResource('posts')}>
+                        Posts
+                    </button>
+                    <button onClick={() => setResource('todos')}>
+                        ToDos
+                    </button>
                 </div>
-                {this.state.resource}
+                {resource}
             </div>
         );
-    }
+   // }
 }
 
 export default App;
