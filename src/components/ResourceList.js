3d9441dfd4c21e55f16c1f5ca73d9457d82d423f
Rodrigo Chavez <rodctech@gmail.com>
2019-05-31 09:39:11 -0500

    323- Refactoring to FunctionalComp

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,16 +1,9 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import axios from 'axios';
 
-
+/*  REFACTOR INTO FUNCTIONAL Comp
 class ResourceList extends Component {
     state = {resources: [ ]};
-   async componentDidMount() {
-        const response = await axios.get(
-            `https://jsonplaceholder.typicode.com/${this.props.resource}`
-        );
-
-       this.setState({resources: response.data})
-    }
 
     async componentDidUpdate(prevProps) {
        if(prevProps.resource !== this.props.resource) {
@@ -21,13 +14,26 @@ class ResourceList extends Component {
        }
     }
 
-    render() {
+    async componentDidMount() {*/
+const ResourceList = () => {
+    const [resources, setResources] = useState([]);
+
+    const fetchResource = async () => {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        );
+
+        this.setState({resources: response.data});
+    };
+
+   // render() {
         return (
             <div>
-                {this.state.resources.length}
+                {resources.length}
             </div>
         );
-    }
-}
+   // }
+};
+
 
 export default ResourceList;
